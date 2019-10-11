df0867fbee8c5adbbcfdf39cf916224e1590d70e
lizhongzhen11 <295630519@qq.com>
2019-10-11 11:07:28 +0800

    --准备发包

---
+++ README.md
@@ -0,0 +1,47 @@
+# react-cascader-transfer
+
+## 预览
+<img src="./GIF.gif">
+
+## 安装
+```js
+npm install react-cascader-transfer --save-dev
+```
+
+## 用法
+```tsx
+import ReactCascaderTransfer from 'react-cascader-transfer'
+import city from './config/city'
+
+const handleOnchange = (selected, value) => {
+  console.log(selected, value)
+}
+
+<ReactCascaderTransfer dataSource={city} titles={['省级', '市级']} value={['11', '120103', '513300']} onChange={handleOnchange}/>,
+```
+
+## API
+见 **interface.ts**
+```ts
+interface CascaderTransferProps {
+  /** 指定选中项 */
+  value?: Array<number | string>;
+  /** 列头名 */
+  titles?: string[];
+  /** 自定义样式 */
+  style?: React.CSSProperties;
+  /** 自定义类名 */
+  className?: string;
+  /** 自定义已选框宽度 */
+  selectedWidth?: number | string;
+  /** 数据源 */
+  dataSource: DataProps[];
+  /** 禁用*/
+  disabled?: boolean; // 待开发
+  /** 自定义每列宽度 */
+  width?: string | number;
+  /** 已选中数据 */
+  selected?: DataProps[];
+  onChange: (selected: DataProps[], value: Array<number | string>) => void
+}
+```
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/test.tsx", // 本地测试
+  entry: "./src/index.tsx", // 本地测试
   // entry: "./src/index.tsx", // 打包
   output: {
     filename: "react-cascader-transfer.js",
@@ -44,18 +44,18 @@ module.exports = {
       template: path.resolve(__dirname, 'index.html')
     })
   ],
-  // externals: { // 打包到生产并发布到npm上需要开启，因为用到了react hooks，不然会因为有两个react副本而导致hooks报错
-  //   react: {
-  //     root: 'React',
-  //     commonjs2: 'react',
-  //     commonjs: 'react',
-  //     amd: 'react'
-  //   },
-  //   'react-dom': {
-  //     root: 'ReactDOM',
-  //     commonjs2: 'react-dom',
-  //     commonjs: 'react-dom',
-  //     amd: 'react-dom'
-  //   }
-  // },
+  externals: { // 打包到生产并发布到npm上需要开启，因为用到了react hooks，不然会因为有两个react副本而导致hooks报错
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    }
+  },
 };
